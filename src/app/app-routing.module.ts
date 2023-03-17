da780b0dfab59b060c8c636b2fe143b84f53ecd2
AssaNiang <mbayeou576@gmail.com>
2023-03-17 13:26:47 +0100

    j'ai fait les liens entre les pages historique, home et nouvelle tache routerLink,app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+    
+  },
+  {
+    path:"new-task",
+  component:NewTaskComponent
 
-const routes: Routes = [];
+  },
+  {
+    path:"history",
+    component:HistoryComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { HistoryComponent } from './pages/history/history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    NewTaskComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
