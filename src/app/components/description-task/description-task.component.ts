8bcf71a693f491be9819ac326bb0caa76ee5aff6
AssaNiang <mbayeou576@gmail.com>
2023-03-19 23:10:02 +0100

    1er test creation d'une tache avec un textarea et un bouton et l'afficher et le supprimer

--- src/app/components/description-task/description-task.component.ts
+++ src/app/components/description-task/description-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-description-task',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./description-task.component.css']
 })
 export class DescriptionTaskComponent {
-
+  list:any[]=[];
+  addtask(item:string){
+    console.log("la valeur :",item);
+    this.list.push({id:this.list.length,content:item});
+    console.log(this.list);
+    
+  }
+removeTask(id:number){
+  console.log(" le id est de la tache à supprimer est :",id);
+  this.list=this.list.filter(item=>item.id!==id);
+}
 }
