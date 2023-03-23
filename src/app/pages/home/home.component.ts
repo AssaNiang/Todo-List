047cab71d040f8a7111933a07ff776017b6c938c
AssaNiang <mbayeou576@gmail.com>
2023-03-23 18:33:41 +0100

    la page home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,36 +17,35 @@ export class HomeComponent {
 this.getAllTask();
 // this.getUrgentTask();
 // this.getUrgentTask2(false);
-// this.taskUrgent=this.getUrgentTask();
-// this.taskNoUrgent=this.getNoUrgentTask();
+this.taskUrgent=this.getUrgentTask();
+this.taskNoUrgent=this.getNoUrgentTask();
   }
  getAllTask(){
 const AllTask=this.taskservice.getTaskBasket();
 return AllTask;
  }
-// getUrgentTask(){
-//   const AllTask=this.taskservice.getTaskBasket();
-// let UrgentTask=AllTask.filter((UTask:ITask)=>UTask.isUrgent==true);
-// console.log("les taches urgentes",UrgentTask);
-// return UrgentTask;
- 
-// }
-// getNoUrgentTask(){
-//   const AllTask=this.taskservice.getTaskBasket();
-// let UrgentTask1=AllTask.filter((UTask:ITask)=>UTask.isUrgent===false);
-// console.log("les taches non urgentes",UrgentTask1);
-// return UrgentTask1;
- 
-// }
-getUrgentTask(valid:boolean){
-  this.taskList=this.getAllTask();
-  console.log(this.taskList);
-  console.log(this.getAllTask());
-
-  let UrgentTask =this.taskList.filter((UTask)=>UTask.isUrgent===valid);
-  console.log(UrgentTask);
-  console.log(valid);
+getUrgentTask(){
+  const AllTask=this.taskservice.getTaskBasket();
+let UrgentTask=AllTask.filter((UTask:ITask)=>UTask.isUrgent==true);
+console.log("les taches urgentes",UrgentTask);
+console.log("le tableau des taches",this.taskList);
 return UrgentTask;
+ 
 }
+getNoUrgentTask(){
+  const AllTask=this.taskservice.getTaskBasket();
+let UrgentTask1=AllTask.filter((UTask:ITask)=>UTask.isUrgent===false);
+console.log("les taches non urgentes",UrgentTask1);
+return UrgentTask1;
+ 
+}
+// getUrgentTask(valid:boolean){
+//   this.taskList=this.getAllTask();
+//   let UrgentTask =this.taskList.filter((UTask)=>UTask.isUrgent===valid);
+//  console.log("le tableau",this.taskList);
+//  console.log("UrgentTask",UrgentTask);
+
+// return UrgentTask;
+// }
 
 }
